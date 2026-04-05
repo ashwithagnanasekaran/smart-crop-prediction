f84880b76822fc2d6e0cd56aa358306b1aa7e1c3
Varshika <varshi2923@gmail.com>
2026-04-05 18:44:53 +0530

    added weather api working code

--- varshiks_api.py
+++ varshiks_api.py
@@ -22,3 +22,12 @@ if __name__ == '__main__':
 
 weather = requests.get("https://api.open-meteo.com/v1/forecast?latitude=11&longitude=78&current_weather=true").json()
 print(weather["current_weather"])
+import requests
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=11&longitude=78&current_weather=true"
+response = requests.get(url)
+
+data = response.json()
+
+print("Temperature:", data["current_weather"]["temperature"])
+print("Wind Speed:", data["current_weather"]["windspeed"])
