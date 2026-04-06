65dc47dbc70482cc11867337d417c341b0ee139a
Varshika <varshi2923@gmail.com>
2026-04-06 20:42:21 +0530

    add wind speed extraction from API data

--- varshiks_api.py
+++ varshiks_api.py
@@ -76,3 +76,4 @@ params["current_weather"] = True
 response = requests.get(url, params=params)
 weather_data = data.get("current_weather", {})
 temperature = weather_data.get("temperature", 0)
+wind_speed = weather_data.get("windspeed", 0)
